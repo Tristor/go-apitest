85a612c9261da806824660822d8e27f70bb51b03
tristor <tristor@gmail.com>
2015-11-29 20:15:22 -0700

    prelim flesh out of group functions, need to add in cross-checking for consistency

--- mockdb.go
+++ mockdb.go
@@ -6,6 +6,7 @@ import (
 )
 
 var users Users
+var groups Groups
 
 // Generate seed data
 
@@ -53,4 +54,47 @@ func DBUpdateUser(u User) (User, error) {
 		return u, err
 	}
 	return u, nil
+}
+
+func DBCreateGroup(g Group) (Group, error) {
+	f := DBFindGroup(g.GroupName)
+	if f.GroupName == g.GroupName {
+		err := errors.New("Cannot create a duplicate group")
+		return f, err
+	}
+	groups = append(groups, g)
+	return g, nil
+}
+
+func DBFindGroup(groupname string) Group {
+	for _, g := range groups {
+		if g.GroupName == groupname {
+			return g
+		}
+	}
+	// return an empty Group object if not found
+	return Group{}
+}
+
+func DBDeleteGroup(groupname string) error {
+	for i, g := range groups {
+		if g.GroupName == groupname {
+			groups = append(groups[:i], groups[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("Could not find group %s to delete", groupname)
+}
+
+func DBUpdateGroup(g Group) (Group, error) {
+	groupname := g.GroupName
+	err := DBDeleteGroup(groupname)
+	if err != nil {
+		return g, fmt.Errorf("Could not find user %s to update", groupname)
+	}
+	g, err = DBCreateGroup(g)
+	if err != nil {
+		return g, err
+	}
+	return g, nil
 }
